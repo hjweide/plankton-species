753a24b19d003000616e8a2acbecc8ed3d21ffa8
Hendrik Weideman <weideh@rpi.edu>
2016-03-21 10:31:06 -0400

    Fixed typo.

--- pyplankton.py
+++ pyplankton.py
@@ -348,5 +348,5 @@ if __name__ == '__main__':
 
     app.run(
         host=app.config.get('HOST', '127.0.0.1'),
-        port=app.config.get('POST', 5000)
+        port=app.config.get('PORT', 5000)
     )
